8a062156882d2eb1dcc986c9724939fff97185e6
zhibinliu <wb-zhibinliu@sohu-inc.com>
2013-10-12 17:38:21 +0800

    这些文件都是已经没有用的，不在跟踪的文件

--- application/views/Index_center.php
+++
@@ -1,7 +0,0 @@
- <div class=" offset1 span9 " >
- <iframe  name="index_center" src="<?php echo base_url('index.php/index/center');?>" frameborder="0" width="740" height="740" ></iframe>
- </div>
- </div>
-<!--  </div> -->
- </body>
- </html>
